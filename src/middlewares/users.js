c87cee62ef27dcc1b9a8cbd02cb2f72378277214
Rayron Rodffer <r.rodffer@gmail.com>
2022-01-27 21:05:33 -0300

    Feat: adicionado rota para remover usuario

--- src/middlewares/users.js
+++ src/middlewares/users.js
@@ -16,7 +16,7 @@ async function updateUser(req, res, next) {
 
     if(userId === id || isAdmin){
       let { password } = req.body;
-      
+
       if(password){
         try {
           req.body.password = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT));
@@ -44,6 +44,37 @@ async function updateUser(req, res, next) {
   }
 };
 
+async function deleteUser(req, res, next) {
+  try {
+    const { id } = req.params;
+
+    const existsUser = await User.exists({ id });
+
+    if(!existsUser){
+      return res.status(400).json({ error: 'Usuário não encontrado'});
+    }
+
+    const { userId, isAdmin } = req.body;
+
+    if(userId === id || isAdmin){
+      const response = await User.deleteOne({id});
+
+      if(!response){
+        return res.status(400).json({message: 'Não foi possível remover o usuário'});
+      }
+
+      return res.status(200).json({message: 'Usuário removido com sucesso'});
+    } else {
+      return res.status(403).json({ error: 'Usuário não tem autorização para a solicitação requisitada'});
+    }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({error: 'Erro interno do servidor'});
+  }
+};
+
 module.exports ={
-  updateUser
+  updateUser,
+  deleteUser
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const userMiddleware = require('../middlewares/users');
 
 router.put('/:id', userMiddleware.updateUser);
+router.delete('/:id', userMiddleware.deleteUser);
 
 module.exports = router;
