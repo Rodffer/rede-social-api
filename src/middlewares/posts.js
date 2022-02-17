3b047323d72636da56cacc78caadf3b9fb5544ab
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-16 21:43:38 -0300

    Feat: add rota de busca dos posts do perfil

--- src/middlewares/posts.js
+++ src/middlewares/posts.js
@@ -138,11 +138,27 @@ async function getTimelinePost(req, res, next) {
   }
 };
 
+async function getProfilePost(req, res, next) {
+  try {
+    const { username } = req.params;
+    
+    const user = await User.findOne({ userName: username});
+
+    const posts = await Post.find({ userId: user._id});
+
+    return res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({error: 'Erro interno do servidor'});
+  }
+};
+
 module.exports ={
   createPost,
   updatePost,
   deletePost,
   likePost,
   getPost,
-  getTimelinePost
+  getTimelinePost,
+  getProfilePost
 }
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -8,5 +8,6 @@ router.delete('/:id', postMiddleware.deletePost);
 router.put('/:id/like', postMiddleware.likePost);
 router.get('/:id', postMiddleware.getPost);
 router.get('/timeline/:userId', postMiddleware.getTimelinePost);
+router.get('/profile/:username', postMiddleware.getProfilePost);
 
 module.exports = router;
